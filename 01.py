690fcf8b1fd35dacf8455b4d2502d6f9f6902b4b
olympichek <olympichek1@gmail.com>
2021-01-25 14:03:55 +0200

    simple calculator added

--- 01.py
+++ 01.py
@@ -1,1 +1,12 @@
-print("Hello, World!")
+operators = {
+    '+': lambda x, y: x + y,
+    '-': lambda x, y: x - y,
+    '*': lambda x, y: x * y,
+    '/': lambda x, y: x / y,
+    '^': lambda x, y: x ** y
+}
+
+expr = input('Enter expression: ')
+a_str, op, b_str = expr.split()
+a, b = float(a_str), float(b_str)
+print(operators[op](a, b))
