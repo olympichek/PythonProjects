22bb4adf16339f0620c88227e9ea66bd54737da9
olympichek <olympichek1@gmail.com>
2021-06-10 11:06:07 +0300

    kangaroo added

---
+++ BadKangaroo.py
@@ -0,0 +1,34 @@
+class Kangaroo:
+    """A Kangaroo is a marsupial."""
+    
+    def __init__(self, name, contents=[]):
+        """Initialize the pouch contents.
+        name: string
+        contents: initial pouch contents.
+        """
+        self.name = name
+        self.pouch_contents = contents
+
+    def __str__(self):
+        """Return a string representaion of this Kangaroo.
+        """
+        t = [ self.name + ' has pouch contents:' ]
+        for obj in self.pouch_contents:
+            s = '    ' + obj.__str__()
+            t.append(s)
+        return '\n'.join(t)
+
+    def put_in_pouch(self, item):
+        """Adds a new item to the pouch contents.
+        item: object to be added
+        """
+        self.pouch_contents.append(item)
+    
+
+kanga = Kangaroo('Kanga')
+roo = Kangaroo('Roo')
+kanga.put_in_pouch('wallet')
+kanga.put_in_pouch('car keys')
+kanga.put_in_pouch(roo)
+
+print(kanga)
